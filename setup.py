70b7bc9f3a225dee769f311666a181e26d132661
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2021-12-21 20:41:04 +0000

    Fixed setup

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name="crypto_pnl",
     version="0.1.0",
-    py_modules=["crypto_pnl"],
+    packages=["crypto_pnl"],
     test_suite="",
-    entry_points="""
-        [console_scripts]
-        crypto_pnl=cypto_pnl.__main__:main
-    """,
+    entry_points={
+        'console_scripts': [
+            'crypto_pnl=crypto_pnl.__main__:main',
+        ],
+    },
 )
