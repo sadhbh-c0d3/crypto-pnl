4edc90bb49cb946862a3c003fbc861e730fd02ba
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2021-12-22 13:27:19 +0000

    Correct export to CSV. Fixed fee gains calculation. (There can be gains from paying fees_

--- crypto_pnl/csv_export.py
+++ crypto_pnl/csv_export.py
@@ -1,4 +1,5 @@
 from .core import *
+from .asset import Asset, zero_asset
 from .trade import load_trades
 from .market_data import load_market_data
 from .wallet import Wallet
@@ -9,6 +10,42 @@ from .tracker import Trackers
 from .last_prices import LastPrices
 from .exchange_rate_calculator import ExchangeRateCalculator
 from .console_report import ConsoleReport
+
+
+def render_tracker_event(e):
+    etype, action, data = e
+    if etype == MATCH_EVENT:
+        buy, sell, fee = data
+        data = buy
+        if action in (BUY_MATCH_ACTION, SELL_MATCH_ACTION):
+            acquired_value = buy.value_data
+            disposed_value = sell.value_data
+        else:
+            acquired_value = buy.value_data
+            disposed_value = fee.value_data
+        gains = disposed_value - acquired_value
+
+    elif etype == CARRY_EVENT:
+        if action in (STACK_CARRY_ACTION, UNPAID_FEE_CARRY_ACTION):
+            acquired_value = data.value_data
+            disposed_value = 0
+        else:
+            acquired_value = 0
+            disposed_value = data.value_data
+        gains = 0
+    
+    return (
+        etype,
+        action,
+        data.quantity,
+        data.symbol,
+        acquired_value,
+        FIAT_SYMBOL,
+        disposed_value,
+        FIAT_SYMBOL,
+        gains,
+        FIAT_SYMBOL,
+    )
 
 
 def export_trades(trades_path, market_data_paths):
@@ -23,44 +60,59 @@ def export_trades(trades_path, market_data_paths):
         map(load_market_data, market_data_paths))
 
     trades = load_trades(trades_path)
-    print ','.join((
-            'transaction',
-            'date',
-            'market',
-            'action',
-            'asset',
-            'amount',
-            'cost ({})'.format(FIAT_SYMBOL),
-    ))
+    print(','.join((
+        'number',
+        'date',
+        'pair',
+        'side',
+        'main.quantity',
+        'main.symbol',
+        'traded.quantity',
+        'traded.symbol',
+        'fee.quantity',
+        'fee.symbol',
+        'price',
+        'price.symbol',
+        'exchange_via',
+        'exchange_rate',
+        'currency',
+        'trade.value',
+        'currency',
+        'event.type',
+        'event.action',
+        'event.quantity',
+        'event.symbol',
+        'acquired.value',
+        'currency',
+        'disposed.value',
+        'currency',
+        'gains.value',
+        'currency',
+    )))
 
     for number, trade in enumerate(trades):
         exchange_rate_calculator.will_execute(trade)
         journal.execute(trade)
-        print ','.join(map(str,(
-            number,
-            trade.date,
-            trade.pair,
-            get_traded_action(trade.side),
-            trade.executed.symbol,
-            trade.executed.quantity * trade.side,
-            display_fiat(trade.executed.value * trade.side),
-        )))
-        print ','.join(map(str,(
-            number,
-            trade.date,
-            trade.pair,
-            get_main_action(trade.side),
-            trade.amount.symbol,
-            trade.amount.quantity * -trade.side,
-            display_fiat(trade.amount.value * -trade.side),
-        )))
-        print ','.join(map(str,(
-            number,
-            trade.date,
-            trade.pair,
-            FEE_ACTION,
-            trade.fee.symbol,
-            -trade.fee.quantity,
-            display_fiat(-trade.fee.value),
-        )))
-
+        for symbol, tracker in sorted_items(
+                journal.last_transaction.trackers.trackers):
+            for te in tracker.events:
+                print(','.join(map(str, (
+                    number,
+                    trade.date,
+                    trade.pair,
+                    get_side(trade.side),
+                    trade.amount.quantity,
+                    trade.amount.symbol,
+                    trade.executed.quantity,
+                    trade.executed.symbol,
+                    trade.fee.quantity,
+                    trade.fee.symbol,
+                    trade.price,
+                    trade.amount.symbol,
+                    trade.exchange_symbol,
+                    trade.exchange_rate,
+                    FIAT_SYMBOL,
+                    trade.amount.value_data,
+                    FIAT_SYMBOL,
+                ) + render_tracker_event(te)
+                )))
