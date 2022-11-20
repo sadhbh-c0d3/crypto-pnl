76f8c5ebdd2a6cf0fb2d7c02ee7816e4dde88245
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2022-11-20 17:23:36 +0000

    export-fifo updated

--- export-fifo.sh
+++ export-fifo.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-python3 -m crypto_pnl export-tracker-events-fifo ../data/raw_data > ../data/output/tracker-fifo.csv
-python3 -m crypto_pnl export-trades ../data/raw_data > ../data/output/trades.csv
-python3 -m crypto_pnl export-ledger ../data/raw_data > ../data/output/ledger.csv
-python3 -m crypto_pnl export-prices ../data/raw_data > ../data/output/prices.csv
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+if [ ! -e $INPUT_DIR ]
+then
+    echo "No such directory $INPUT_DIR"
+    exit 1
+fi
+
+if [ ! -e $OUTPUT_DIR ]
+then
+    mkdir -p $OUTPUT_DIR
+fi
+
+python3 -m crypto_pnl export-tracker-events-fifo $INPUT_DIR > $OUTPUT_DIR/tracker-fifo.csv
+python3 -m crypto_pnl export-trades $INPUT_DIR > $OUTPUT_DIR/trades.csv
+python3 -m crypto_pnl export-ledger $INPUT_DIR > $OUTPUT_DIR/ledger.csv
+python3 -m crypto_pnl export-prices $INPUT_DIR > $OUTPUT_DIR/prices.csv
